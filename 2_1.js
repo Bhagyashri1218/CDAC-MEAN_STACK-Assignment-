4f17ede8911ebcfc72b78ab89aa8bb9d2f64e262
Bhagyashri Deore <31219332+Bhagyashri1218@users.noreply.github.com>
2020-08-26 21:36:02 +0530

    Add files via upload

---
+++ 2_1.js
@@ -0,0 +1,28 @@
+
+let callAjax = () => {
+    console.log("CALLING AJAX HERE!!!");
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://fakerestapi.azurewebsites.net/api/Authors");
+
+    xhr.onload = () => {
+        console.log(xhr.responseText);
+        const refjson = JSON.parse(xhr.responseText);
+        handleDOMOperatoinUsingJSON(refjson);
+    };
+
+    xhr.send();
+};
+
+let handleDOMOperatoinUsingJSON = (refjson) => {
+
+    for (let i = 0; i < refjson.length; i++) {
+        let item = refjson[i];
+        console.log(item);
+        const parent = document.querySelector("#parent");
+        const newElement = parent.children[0].cloneNode(true);
+        newElement.innerHTML = item.FirstName + " " + item.LastName;
+        parent.insertBefore(newElement, parent.firstChild);
+    }
+};
+
